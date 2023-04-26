68d81c97029f1b6bc7d2235ab75dac1aaf7cd2fc
Dhruv Mohapatra <dhrvmohapatra@gmail.com>
2023-04-26 12:13:11 +0530

    error handle and rate limit socket

--- src/websockets/index.js
+++ src/websockets/index.js
@@ -2,6 +2,13 @@ const ws = require('ws');
 
 require('dotenv').config()
 const { PORT } = process.env;
+const { RateLimiterMemory } = require('rate-limiter-flexible');
+
+// Create a rate limiter to limit the number of connections per IP address
+const limiter = new RateLimiterMemory({
+    points: 10, // Maximum number of connections
+    duration: 1, // Per second
+});
 
 module.exports = function (app, db) {
     const DriverLive = db.driverLive;
@@ -10,14 +17,14 @@ module.exports = function (app, db) {
         path: '/websocket',
     });
 
-    wsServer.on('connection', (ws, _) => {
-        console.log('WebSocket connection established');
+    wsServer.on('connection', (ws, req) => {
+        console.log(`WebSocket connection established for client ${req.socket.remoteAddress}`);
 
         // When we receive GPS data from the client, update the driver's live location in the database
         ws.on('message', async (message) => {
-            const { driverPhone, latitude, longitude } = JSON.parse(message);
-
             try {
+                const { driverPhone, latitude, longitude } = JSON.parse(message);
+
                 await DriverLive.findOneAndUpdate(
                     { driverPhone },
                     { location: { type: 'Point', coordinates: [longitude, latitude] } },
@@ -27,7 +34,15 @@ module.exports = function (app, db) {
                 console.log(`Live location updated for driver ${driverPhone}`);
             } catch (err) {
                 console.error(err);
+
+                // Send an error message to the client if JSON parsing fails
+                ws.send(JSON.stringify({ error: 'Invalid message format' }));
             }
+        });
+
+        // When the WebSocket connection is closed, log the event
+        ws.on('close', (code, reason) => {
+            console.log(`WebSocket connection closed for client ${req.socket.remoteAddress}: code=${code}, reason=${reason}`);
         });
     });
 
@@ -36,9 +51,25 @@ module.exports = function (app, db) {
     });
 
     server.on('upgrade', (request, socket, head) => {
-        wsServer.handleUpgrade(request, socket, head, socket => {
-            wsServer.emit('connection', socket, request);
-        });
+        limiter.consume(request.socket.remoteAddress)
+            .then(() => {
+                wsServer.handleUpgrade(request, socket, head, socket => {
+                    wsServer.emit('connection', socket, request);
+                });
+            })
+            .catch((rejRes) => {
+                // If the limit is exceeded, reject the WebSocket connection
+                socket.write(`HTTP/1.1 429 Too Many Requests\r\nContent-Length: ${rejRes.msBeforeNext}\r\nRetry-After: ${Math.ceil(rejRes.msBeforeNext / 1000)}\r\n\r\n`);
+                socket.destroy();
+            });
+    });
+
+    wsServer.on('close', () => {
+        console.log('WebSocket server closed');
+    });
+
+    wsServer.on('error', (err) => {
+        console.error(`WebSocket server error: ${err}`);
     });
 
     return server;
